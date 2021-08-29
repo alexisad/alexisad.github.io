49cd7b869cf288ecef81026255368fee7c1d38de
Alexander Sadovoy <alexander.sadovoy@here.com>
2021-08-29 22:03:42 +0200

    CHG

--- split-sectors/types.nim
+++ split-sectors/types.nim
@@ -1,12 +1,24 @@
 import utils, strutils, tables, unicode, sugar, hashes
-import nre except toSeq
+#import nre except toSeq
+
+export hashes
 
 type
+    City* = ref object of Rootref
+        id*: string
+        name*: string
+        pdeName*: seq[PdeName]
     District* = ref object of Rootref
         id*: string
         name*: string
+        pdeName*: seq[PdeName]
         polygonOuter*: seq[float]
         outerPoints*: seq[Point]
+    AdminStreet* = ref object of Rootref
+        city*: City
+        postalCode*: string
+        district*: District
+        street*: string
     TileId* = object
         x*, y*: int
     TileLayerSet* = ref object of Rootref
@@ -17,9 +29,13 @@ type
         layrs*: seq[seq[string]]
         levels*: seq[seq[int]]
         tileXys*: seq[seq[string]]
+    Area* = ref object of Rootref
+        roadLinks*: TableRef[string, RoadLink]
+        cities*: TableRef[string, City]
+        districts*: TableRef[string, District]
     RoadLink* = ref object of Rootref
         linkId*: string
-        name*: seq[StreetName]
+        name*: seq[PdeName]
         disstrictId*: string
         cityId*: string
         coords*: seq[Point]
@@ -27,40 +43,67 @@ type
         refLinks*: seq[string]
         nonRefLinks*: seq[string]
         postalCode*: string
-    StreetNameType* = enum
-        abbreviation = 'A', baseName = 'B',
-        exonym = 'E', shortenedName = 'K',
-        synonym = 'S'
-    StreetNameKind* = enum
+    PdeNameType* = enum
+        abbreviation = "A - abbreviation", baseName = "B - base name",
+        exonym = "E - exonym", shortenedName = "K - shortened name",
+        synonym = "S - synonym", unknown = "unknown"
+    PdeNameKind* = enum
         name, translit, phoneme
-    StreetName* = ref object of Rootref
+    PdeName* = ref object of Rootref
         name*: string
         lang*: LangCode
-        nameType*: StreetNameType
-        nameKind*: StreetNameKind
+        nameType*: PdeNameType
+        nameKind*: PdeNameKind
     LangCode* = distinct string
 
+proc encodeName*(it: seq[PdeName], lang = "GER".LangCode): string
 
 proc hashCityPc*(x: RoadLink): Hash =
     result = x.cityId.hash !& x.postalCode.hash
     result = !$result
 
+proc hash*(x: LangCode): Hash =
+    result = string(x).hash
+    result = !$result
 
+proc hash*(x: AdminStreet): Hash =
+    ## Piggyback on the already available string hash proc.
+    ##
+    ## Without this proc nothing works!
+    result = x.postalCode.hash !& x.city.pdeName.encodeName.hash !&
+                x.district.pdeName.encodeName.hash !& x.street.hash
+    result = !$result
 
-proc parseStreetNames*(strEnc: string): seq[StreetName] {.inline.} =
+proc getEnumTypeStreet(x: char): PdeNameType =
+    case x
+    of 'A':
+        result = PdeNameType.abbreviation
+    of 'B':
+        result = PdeNameType.baseName
+    of 'E':
+        result = PdeNameType.exonym
+    of 'K':
+        result = PdeNameType.shortenedName
+    of 'S':
+        result = PdeNameType.synonym
+    else:
+        result = PdeNameType.unknown
+    
+
+proc parsePdeNames*(strEnc: string): seq[PdeName] {.inline.} =
     #echo "strEnc:", strEnc
     let arrLngStr = strEnc.split '\x1d'
     #if arrLngStr.len > 0:
         #echo "arrLngStr:", arrLngStr
     for lng in arrLngStr:
         let arrName = lng.split '\x1e'
-        var streetName: StreetName
+        var pdeName: PdeName
         if arrName.len > 0:
             let
                 lngCode = arrName[0].runeSubStr(0, 3)
-                nameTyp = StreetNameType arrName[0].runeSubStr(3, 1)[0]
+                nameTyp = getEnumTypeStreet arrName[0].runeSubStr(3, 1)[0]
                 nameTxt = arrName[0].runeSubStr(5)
-            streetName = StreetName(name: nameTxt, lang: lngCode.LangCode, nameType: nameTyp)
+            pdeName = PdeName(name: nameTxt, lang: lngCode.LangCode, nameType: nameTyp)
             #echo "arrName[0]:", arrName[0]
             #echo "lngCode:", lngCode
             #echo "nameTyp:", nameTyp
@@ -88,19 +131,19 @@ proc parseStreetNames*(strEnc: string): seq[StreetName] {.inline.} =
                         nameTxtPhon = arrPhonem[0].runeSubStr(5)
                     echo "nameTxtPhon:", nameTxtPhon
                     #discard
-        result.add streetName
+        result.add pdeName
         #echo "arrName:", arrName
 
 
-proc streetName*(it: RoadLink, lang = "GER".LangCode): string =
-    var street = collect(initTable()):
-        for s in it.name:
+proc encodeName*(it: seq[PdeName], lang = "GER".LangCode): string =
+    var street = collect(initTable):
+        for s in it:
             {s.lang.string: s.name}
-    for s in it.name:
-        echo "strName:", s.lang.string, " ", s.name, " ", s.nameType, " ", s.nameKind
-        if s.nameType != StreetNameType.baseName:
+    for s in it:
+        #echo "strName:", s.lang.string, " ", s.name, " nameType:", s.nameType, " nameKind:", s.nameKind
+        if s.nameType != PdeNameType.baseName:
             continue
-        if s.nameKind != StreetNameKind.name:
+        if s.nameKind != PdeNameKind.name:
             continue
         if street[s.lang.string].len < s.name.len:
             street[s.lang.string] = s.name
@@ -114,3 +157,4 @@ proc streetName*(it: RoadLink, lang = "GER".LangCode): string =
     for s in street.values:
         result = s
         break
+
